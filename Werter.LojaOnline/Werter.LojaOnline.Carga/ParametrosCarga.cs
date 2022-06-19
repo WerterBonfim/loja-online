ba5ac3cf716246e40949396a8fbbb8b366a4f9a3
Werter <werter@hotmail.com.br>
2022-06-19 00:37:43 -0300

    Delete Werter.LojaOnline/Werter.LojaOnline.Carga directory
    Não existe mais

--- Werter.LojaOnline/Werter.LojaOnline.Carga/ParametrosCarga.cs
+++
@@ -1,9 +0,0 @@
-namespace Werter.LojaOnline.Carga;
-
-public class ParametrosCarga
-{
-    public string StringDeConexao { get; set; }
-    public int QuantidadeDeRegistros { get; set; }
-    public string NomeDaTabela { get; set; }
-    
-}
